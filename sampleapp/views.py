f411d64bf099834f6743bcddf6dc90a31fb75739
Muhammed Asad <asadnasar748@gmail.com>
2025-05-14 19:45:38 +0530

    Updated navbar design

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,12 +1,6 @@
-from lib2to3.fixes.fix_input import context
-
-from django.template.context_processors import request
-
-from . import models
-
 from django.shortcuts import render
 
-from .models import Main_slidebar
+from . import models
 
 
 def index(request):
@@ -16,37 +10,40 @@ def index(request):
         "columns": models.Article.objects.order_by('?')[:3],
     }
     print(context_data)
-    return render(request, "index.html",context_data)
+    return render(request, "index.html", context_data)
 
 
 def articles(request, pk):
     context_data = {
         "article": models.Article.objects.filter(id=pk).first(),
     }
-    return render(request, "articles.html",context_data)
+    return render(request, "articles.html", context_data)
 
 
 def contents(request):
     context_data = {
         "articles": models.Article.objects.all(),
     }
-    return render(request, "contents.html",context_data)
+    return render(request, "contents.html", context_data)
 
-def Main_slidebar(request):
+
+def main_slidebar(request):
     context_data = {
         "Mainslidebarcd sa": models.Main_slidebar.objects.first()
     }
-    return render(request, "Main_slidebar.html",context_data)
+    return render(request, "Main_slidebar.html", context_data)
+
 
 def trending(request):
     context_data = {
 
         "articles": models.Article.objects.all(),
     }
-    return render(request, "trending.html",context_data)
+    return render(request, "trending.html", context_data)
+
 
 def exclusive_insight(request):
     context_data = {
 
     }
-    return render(request, "exclusive_insight.html",context_data)
+    return render(request, "exclusive_insight.html", context_data)
